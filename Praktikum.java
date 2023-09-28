1e1392fb94a75796ad82200a7be7cc03912c504e
Sonya <stolbsonya@yandex.ru>
2023-09-28 23:39:45 +0200

    Добавлен метод main

--- Praktikum.java
+++ Praktikum.java
@@ -1,26 +1,4 @@
-public class Hamster{
-	String name = "Байт"; // Имя
-    	int age = 2; // Возраст
-    	String color = "Рыжий"; // Цвет
-    	int weight = 350; // Вес
-
-
-	// Методы
-    	// Есть
-    	void eat(int foodWeight) {
-        weight = weight + foodWeight;
-    	} 
-    
-    	// Бегать в колесе
-    	void runInWheel() {
-        System.out.println("Бегу-бегу-бегу!");
-        weight = weight - 5;
-    	}
-    
-    	// Прятать семечки
-    	void hideSeeds(int seedWeight) {
-        weight = weight + seedWeight;
-        System.out.println("Зимой не заголодаю.");
-    	}
-	} 
+public class Praktikum{
+public static void main(String[] args){
+}
 }
